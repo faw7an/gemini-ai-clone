bd1969ee6d092bc5732745e26dc11c07c4139917
Fauzan Said <fauzdasoodais@gmail.com>
2024-10-15 23:44:21 +0300

    Refactor Context component: Add onSent function for chat prompt handling

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,11 +1,14 @@
 import {createContext} from 'react';
-
+import runChat from '../config/gemini'
 export const Context = createContext();
 
 const ContextProvider = (props)=>{
-
+    const onSent = async=>(prompt){
+        await runChat(prompt);
+        
+    }
     const contextValue = {
-
+        
     }
     return(
         <Context.Provider value={contextValue}>
